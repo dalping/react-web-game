c00a46d2db44301931e40e291bb4fe2fddd1f922
Romini's coding <74459925+dalping@users.noreply.github.com>
2021-12-20 20:32:22 +0900

    Update Omok.js

--- games/src/components/Omok/Omok.js
+++ games/src/components/Omok/Omok.js
@@ -1,6 +1,21 @@
 import React from 'react'
 
 function Omok() {
+
+    const [state, setstate] = useState([])
+
+    useEffect(() => {
+        let arr = []
+        for(let i=0;i<15;i++){
+            let row = []
+            for(let j=0;j<15;j++){
+                row.push(null)
+            }
+        }
+
+    }, [])
+
+
     return (
         <div className="game">
             <div className="board">
